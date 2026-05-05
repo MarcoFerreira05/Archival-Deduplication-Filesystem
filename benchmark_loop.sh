cf575042aba1a8207ae5d6426e4910a5031e30f9
MarcoFerreira05 <a106857@alunos.uminho.pt>
2026-05-05 11:10:02 +0100

    Add statistics calculations at the end of the loop script

--- benchmark_loop.sh
+++ benchmark_loop.sh
@@ -5,3 +5,13 @@ for i in $(seq 1 $1); do
 	sleep 10
 done
 
+echo ""
+echo "WARNING: Statistics will be calculated using every result sub-directory inside results/"
+
+cd results/
+python3 ../benchmarks/csv_aggregator_syscounter.py "passthrough.*" 2026-*/passthrough_results.csv passthrough_results.json
+python3 ../benchmarks/csv_aggregator_syscounter.py "python3.*" 2026-*/workload_results.csv workload_results.json
+python3 ../benchmarks/json_aggregator_cachestat.py 2026-*/cachestat_passthrough.json cachestat_passthrough.json
+cd ..
+
+echo "Statistics successfully calculated!"
